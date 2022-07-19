e6f3dd4c7c18bc89f8c0fb5f0e4fbbdde9dcb8fb
Hugo Tunius <hugo@lookback.io>
2022-07-19 11:51:27 +0100

    Rework stats interceptor

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -1,13 +1,17 @@
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 
-mod receiver;
-mod sender;
-#[cfg(test)]
-mod stats_test;
+mod interceptor;
 
-#[derive(Clone, Debug, Default)]
-struct RTPStats {
+pub use interceptor::StatsInterceptor;
+
+pub fn make_stats_interceptor(id: &str) -> Arc<StatsInterceptor> {
+    Arc::new(StatsInterceptor::new(id.to_owned()))
+}
+
+#[derive(Debug, Default)]
+/// Records stats about a given RTP stream.
+pub struct RTPStats {
     /// Packets sent or received
     packets: Arc<AtomicU64>,
 
@@ -26,15 +30,72 @@ impl RTPStats {
         self.packets.fetch_add(packets, Ordering::SeqCst);
     }
 
+    pub fn reader(&self) -> RTPStatsReader {
+        RTPStatsReader {
+            packets: self.packets.clone(),
+            payload_bytes: self.payload_bytes.clone(),
+            header_bytes: self.header_bytes.clone(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+/// Reader half of RTPStats.
+pub struct RTPStatsReader {
+    packets: Arc<AtomicU64>,
+    payload_bytes: Arc<AtomicU64>,
+    header_bytes: Arc<AtomicU64>,
+}
+
+impl RTPStatsReader {
+    /// Get packets sent or received.
     pub fn packets(&self) -> u64 {
         self.packets.load(Ordering::SeqCst)
     }
 
+    /// Get payload bytes sent or received.
     pub fn header_bytes(&self) -> u64 {
         self.header_bytes.load(Ordering::SeqCst)
     }
 
+    /// Get header bytes sent or received.
     pub fn payload_bytes(&self) -> u64 {
         self.payload_bytes.load(Ordering::SeqCst)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rtp_stats() {
+        let stats: RTPStats = Default::default();
+        let reader = stats.reader();
+        assert_eq!(
+            (
+                reader.header_bytes(),
+                reader.payload_bytes(),
+                reader.packets()
+            ),
+            (0, 0, 0),
+        );
+
+        stats.update(24, 960, 1);
+
+        assert_eq!(
+            (
+                reader.header_bytes(),
+                reader.payload_bytes(),
+                reader.packets()
+            ),
+            (24, 960, 1),
+        );
+    }
+
+    #[test]
+    fn test_rtp_stats_send_sync() {
+        fn test_send_sync<T: Send + Sync>() {}
+        test_send_sync::<RTPStats>();
     }
 }
